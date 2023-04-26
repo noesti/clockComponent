07017851b5e2a5e011c7f3ac3e793fde109da3ed
Noel <96701996+noesti@users.noreply.github.com>
2023-04-26 12:35:23 +0200

    clock new

--- clock.js
+++ clock.js
@@ -146,3 +146,17 @@ export class Clock extends HTMLElement {
 }
 
 customElements.define("custom-clock",Clock);
+
+// function isValidTimeZone(tz) {
+//     if (!Intl || !Intl.DateTimeFormat().resolvedOptions().timeZone) {
+//         throw new Error('Time zones are not available in this environment');
+//     }
+
+//     try {
+//         Intl.DateTimeFormat(undefined, {timeZone: tz});
+//         return true;
+//     }
+//     catch (ex) {
+//         return false;
+//     }
+// }
